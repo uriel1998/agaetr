140b04d9c4563c7a9a0340677f91985fa1137b33
Steven Saus <steven@stevesaus.com>
2019-12-21 15:35:46 -0500

    Fix stupid typo in array

--- agaetr_send.sh
+++ agaetr_send.sh
@@ -3,7 +3,7 @@
 #get install directory
 export SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
-if [ -f "$HOME/.config/agaetr/agaetr.ini" ];then
+if [ ! -f "$HOME/.config/agaetr/agaetr.ini" ];then
     echo "INI not located; betcha nothing else is set up."
     exit 89
 fi
@@ -14,7 +14,7 @@ fi
 
 mv "$HOME/.local/share/agaetr/posts.db" "$HOME/.local/share/agaetr/posts_back.db"
 tail -n +2 "$HOME/.local/share/agaetr/posts_back.db" > "$HOME/.local/share/agaetr/posts.db"
-instring = $(head -1 "$HOME/.local/share/agaetr/posts_back.db")
+instring=$(head -1 "$HOME/.local/share/agaetr/posts_back.db")
 rm "$HOME/.local/share/agaetr/posts_back.db"
 
 OIFS=$IFS
@@ -27,16 +27,18 @@ IFS=$OIFS
 #pulling array into named variables so they work with sourced functions
 
 # passing published time (from dd MMM)
-posttime=$(echo "${myarr[1]}")
+posttime=$(echo "${myarr[0]}")
+echo "${myarr[0]}"
 posttime2=${posttime::-4}
 pubtime=$(date -d"$posttime2" +%d\ %b)
-title=$(echo "${myarr[2]}")
-link=$(echo "${myarr[3]}")
-cw=$(echo "${myarr[4]}")
-imgurl=$(echo "${myarr[5]}")
-imgalt=$(echo "${myarr[6]}")
-hashtags=$(echo "${myarr[7]}")
-description=$(echo "${myarr[8]}")
+echo "$pubtime"
+title=$(echo "${myarr[1]}")
+link=$(echo "${myarr[2]}")
+cw=$(echo "${myarr[3]}")
+imgurl=$(echo "${myarr[4]}")
+imgalt=$(echo "${myarr[5]}")
+hashtags=$(echo "${myarr[6]}")
+description=$(echo "${myarr[7]}")
             
 #Deshortening, deobfuscating, and unredirecting the URL
 
@@ -54,7 +56,8 @@ if [ "$(ls -A "$SCRIPT_DIR/short_enabled")" ]; then
     short_funct=$(echo "${shortener%.*}_shortener")
     source "$SCRIPT_DIR/short_enabled/$shortener"
     url="$link"
-    eval ${short_funct}
+    echo "$SCRIPT_DIR/short_enabled/$shortener"
+    #eval ${short_funct}
     link="$shorturl"
 fi
     
@@ -67,7 +70,8 @@ for p in $posters;do
     echo "Processing ${p%.*}..."
     send_funct=$(echo "${p%.*}_send")
     source "$SCRIPT_DIR/out_enabled/$p"
-    eval ${send_funct}
+    echo "$SCRIPT_DIR/out_enabled/$p"
+    #eval ${send_funct}
 done
 
 
