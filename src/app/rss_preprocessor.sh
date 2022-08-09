1525f13147b2d9374378c7eea4a9f7639adda36f
Steven Saus <steven@stevesaus.com>
2022-08-09 09:13:46 -0400

    Building of commandline good - will need a note re sed and flatpak

--- src/app/rss_preprocessor.sh
+++ src/app/rss_preprocessor.sh
@@ -5,12 +5,13 @@ INI_URL=""
 if [ -f "${XDG_CONFIG_HOME}/agaetr/feeds.ini" ];then
     INI_URL="${XDG_CONFIG_HOME}/agaetr/feeds.ini"
     else
-    if [ -f ${XDG_CONFIG_HOME}/agaetr/agaetr.ini ];then
+    if [ -f "${XDG_CONFIG_HOME}/agaetr/agaetr.ini" ];then
         INI_URL="${XDG_CONFIG_HOME}/agaetr/agaetr.ini"
     fi
 fi
 
-if [ "${INI_URL}" == "" ];then
+echo "${INI_URL}"
+if [ ! -f "${INI_URL}" ];then
     # not running
     exit 90
 else
@@ -18,9 +19,9 @@ else
     # TODO - THIS RIGHT HERE
     # Search ini file in XDG_CONFIG_HOME
     OIFS=$IFS
-IFS=$'\n'
-myarr=($(grep --after-context=2 -e "^src=" "${INI_URL}"))
-IFS=$OIFS
+    IFS=$'\n'
+    myarr=($(grep --after-context=2 -e "^src =" "${INI_URL}"))
+    IFS=$OIFS
     # myarr=$(grep --after-context=2 -e "^src=")
     # 
     # find src/cmd/url trio
@@ -36,16 +37,18 @@ IFS=$OIFS
         thecommand=""
         j=$(( i+1 ))
         k=$(( j+1 ))
-        if [[ "${myarr[$i]" == "src"* ]];then
-            mysrc=$(echo "$myarr[$i]" | awk -F ' = ' '{print $2}')
-            if [[ "${myarr[$j]" == "cmd"* ]];then
-                mycmd=$(echo "$myarr[$j]" | awk -F ' = ' '{print $2}')
-                if [[ "${myarr[$k]" == "url"* ]];then
-                    myurl=$(echo "$myarr[$k]" | awk -F ' = ' '{print $2}')
+        if [[ "${myarr[$i]}" == "src"* ]];then
+            mysrc=$(echo "${myarr[$i]}" | awk -F ' = ' '{print $2}')
+            if [[ "${myarr[$j]}" == "cmd"* ]];then
+                mycmd=$(echo "${myarr[$j]}" | awk -F ' = ' '{print $2}')
+                if [[ "${myarr[$k]}" == "url"* ]];then
+                    myurl=$(echo "${myarr[$k]}" | awk -F ' = ' '{print $2}')
+                    
                     #time to create the command string
-                    thecommand=$(printf "wget -O- \"%s\" | %s > \"%s\"" "${mysrc}" "${mycmd}" "${myurl}")
+                    thecommand=$(printf "wget -O- \"%s\" | %s > \"%s/agaetr%s\"" "${mysrc}" "${mycmd}" "${XDG_DATA_HOME}" "${myurl}")
                     # wget -O- "${src}" | "${cmd}" > "${url}"
-                    eval "${thecommand}"
+                    echo "${thecommand}"
+                    #eval "${thecommand}"
                 fi
             fi
         fi
@@ -58,5 +61,5 @@ IFS=$OIFS
     #wget -O- "https://ideatrash.net/feed" | sed -e 's/<div class="more-link-wrapper">.*\]\]><\/description>/\]\]\><\/description>/g' > $XDG_DATA_HOME/agaetr/feeds/ideatrash_parsed.xml
 
     # <updated> -> <pubDate> (and closing tags)
-    wget -O- "https://my.ttrss.install/public.php?op=rss&id=-2&view-mode=all_articles&key=8293847903" | sed 's@<updated>@<pubDate>@g' | sed 's@</updated>@</pubDate>@g' | sed 's@Media playback is unsupported on your device @@g' > $XDG_DATA_HOME/agaetr/feeds/ttrss_parsed.xml
+    # wget -O- "https://my.ttrss.install/public.php?op=rss&id=-2&view-mode=all_articles&key=8293847903" | sed 's@<updated>@<pubDate>@g' | sed 's@</updated>@</pubDate>@g' | sed 's@Media playback is unsupported on your device @@g' > $XDG_DATA_HOME/agaetr/feeds/ttrss_parsed.xml
 fi
