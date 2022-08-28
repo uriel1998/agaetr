251a9a7aacb0faaf3eedbcaa5f7335d30e17df8d
Steven Saus <steven@stevesaus.com>
2022-08-28 03:39:42 -0400

    started putting in standardization and sourcing in out modes

--- src/app/out_avail/shaarli.sh
+++ src/app/out_avail/shaarli.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+
+##############################################################################
+#
+#  sending script
+#  (c) Steven Saus 2022
+#  Licensed under the MIT license
+#
+##############################################################################
+
 function shaarli_send {
     
     binary=$(grep 'shaarli =' "${XDG_CONFIG_HOME}/agaetr/agaetr.ini" | sed 's/ //g' | awk -F '=' '{print $2}')
@@ -10,9 +19,42 @@ function shaarli_send {
 
     # No length requirements here!
     tags=$(echo "$hashtags"  | sed 's|#||g' )
+    
     # NEED TO ADD CONFIG FILE EXPLICITLY HERE
-    outstring=$(echo "$binary post-link --description \"$description\" --tags \"$tags\" --title \"$title\" --url $link ")
+    
+    if [ -z "${description}" ];
+        outstring=$(echo "$binary post-link --title \"$title\" --url $link ")
+    else
+        outstring=$(echo "$binary post-link --description \"$description\" --tags \"$tags\" --title \"$title\" --url $link ")
+    fi
 
     eval ${outstring} > /dev/null
 }
 
+
+##############################################################################
+# Are we sourced?
+# From http://stackoverflow.com/questions/2683279/ddg#34642589
+##############################################################################
+
+# Try to execute a `return` statement,
+# but do it in a sub-shell and catch the results.
+# If this script isn't sourced, that will raise an error.
+$(return >/dev/null 2>&1)
+
+# What exit code did that give?
+if [ "$?" -eq "0" ];then
+    echo "[info] Function ready to go."
+    OUTPUT=0
+else
+    OUTPUT=1
+    if [ "$#" = 0 ];then
+        echo -e "Please call this as a function or with \nthe url as the first argument and optional \ndescription as the second."
+    else
+        link="${1}"
+        if [ ! -z "$2" ];then
+            title="$2"
+        fi
+        shaarli_send
+    fi
+fi
