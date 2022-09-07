6d729b16c249c09ef067bfbb8f6ac8954fe54bc1
Steven Saus <steven@stevesaus.com>
2022-09-07 18:17:44 -0400

    added path checking for rss preprocessor output

--- src/app/rss_preprocessor.sh
+++ src/app/rss_preprocessor.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+##############################################################################
+#
+#  RSS preprocessor file for agaetr
+#  (c) Steven Saus 2022
+#  Licensed under the MIT license
+#
+# To clean up and standardize some odd RSS elements from (in my case) from 
+# Wordpress and from TT-RSS.  Also included as examples.
+# summary remove <div class="more-link-wrapper"> until</description>
+# wget -O- "https://ideatrash.net/feed" | sed -e 's/<div class="more-link-wrapper">.*\]\]><\/description>/\]\]\><\/description>/g' > $XDG_DATA_HOME/agaetr/feeds/ideatrash_parsed.xml
+# <updated> -> <pubDate> (and closing tags)
+# wget -O- "https://my.ttrss.install/public.php?op=rss&id=-2&view-mode=all_articles&key=8293847903" | sed 's@<updated>@<pubDate>@g' | sed 's@</updated>@</pubDate>@g' | sed 's@Media playback is unsupported on your device @@g' > $XDG_DATA_HOME/agaetr/feeds/ttrss_parsed.xml
+##############################################################################
+
+
 INI_URL=""
 
 if [ -f "${XDG_CONFIG_HOME}/agaetr/feeds.ini" ];then
@@ -15,8 +30,6 @@ if [ ! -f "${INI_URL}" ];then
     # not running
     exit 90
 else
-
-    # TODO - THIS RIGHT HERE
     # Search ini file in XDG_CONFIG_HOME
     OIFS=$IFS
     IFS=$'\n'
@@ -41,27 +54,19 @@ else
                     mycmd=$(echo "${myarr[$j]}" | awk -F ' = ' '{print $2}')
                     if [[ "${myarr[$k]}" == "url"* ]];then
                         myurl=$(echo "${myarr[$k]}" | awk -F ' = ' '{print $2}')
-                        
-                        #time to create the command string
-                        thecommand=$(printf "wget -O- \"%s\" | %s > \"%s/agaetr%s\"" "${mysrc}" "${mycmd}" "${XDG_DATA_HOME}" "${myurl}")
-                        # wget -O- "${src}" | "${cmd}" > "${url}"
-                        
                         # CHECK FOR PATH OF where it writes to!
-                        
-                        echo "${thecommand}"
-                        #eval "${thecommand}"
+                        rel_path=$(printf "%s/agaetr%s" "${XDG_DATA_HOME}" "${myurl}")
+                        just_path=$(realpath "${rel_path}" | awk -F '\/' '{$NF=""}1' | sed 's| |\/|g')
+                        if [ ! -d "${just_path}" ];then
+                            mkdir -p "${just_path}"
+                        fi                        
+                        # time to create the command string
+                        thecommand=$(printf "wget -O- \"%s\" | %s > \"%s/agaetr%s\"" "${mysrc}" "${mycmd}" "${XDG_DATA_HOME}" "${myurl}")
+                        #echo "${thecommand}"
+                        eval "${thecommand}"
                     fi
                 fi
             fi
         fi
     done
-    
-    # To clean up and standardize some odd RSS elements from (in my case) from 
-    # Wordpress and from TT-RSS.  Also included as examples.
-
-    # summary remove <div class="more-link-wrapper"> until</description>
-    #wget -O- "https://ideatrash.net/feed" | sed -e 's/<div class="more-link-wrapper">.*\]\]><\/description>/\]\]\><\/description>/g' > $XDG_DATA_HOME/agaetr/feeds/ideatrash_parsed.xml
-
-    # <updated> -> <pubDate> (and closing tags)
-    # wget -O- "https://my.ttrss.install/public.php?op=rss&id=-2&view-mode=all_articles&key=8293847903" | sed 's@<updated>@<pubDate>@g' | sed 's@</updated>@</pubDate>@g' | sed 's@Media playback is unsupported on your device @@g' > $XDG_DATA_HOME/agaetr/feeds/ttrss_parsed.xml
 fi
