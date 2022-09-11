8fb09a9664caccd99f9ed206dc8aef265dd64134
Steven Saus <steven@stevesaus.com>
2022-09-11 19:26:07 -0400

    added loud function

--- src/app/agaetr_send.sh
+++ src/app/agaetr_send.sh
@@ -2,6 +2,14 @@
 
 #get install directory
 export SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+LOUD=0
+
+function loud() {
+    if [ $LOUD -eq 1 ];then
+        echo "$@"
+    fi
+    
+}
 
 if [ ! -d "${XDG_DATA_HOME}" ];then
     export XDG_DATA_HOME="${HOME}/.local/share"
@@ -20,6 +28,11 @@ if [ ! -f "${XDG_DATA_HOME}/agaetr/posts.db" ];then
 fi
 
 if [ "$1" != "" ];then
+    if [ "$1" == "--verbose" ];then
+        shift
+        LOUD=1
+    fi
+
     # if $1 exists, it's from the single processor, and use that 
     # instead of rotating the db but add it to the posted list
     instring="${@}"
@@ -30,11 +43,8 @@ else
     rm "${XDG_DATA_HOME}/agaetr/posts_back.db"
 fi
 
-
-
 if [ -z "$instring" ];then 
-# TODO - QUIET THIS
-    echo "Nothing to post."
+    loud "Nothing to post."
     exit
 fi
 
