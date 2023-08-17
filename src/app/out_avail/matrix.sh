88af6ce6fb77673e46b24ab451fd10533665f29a
Steven Saus <steven@stevesaus.com>
2023-08-16 23:30:15 -0400

    workings...

--- src/app/out_avail/matrix.sh
+++ src/app/out_avail/matrix.sh
@@ -1,1 +1,19 @@
 use sendmail-matrix
+
+https://github.com/tnyeanderson/sendmail-to-matrix/releases/download/v0.1.2/sendmail-to-matrix
+
+$ sendmail myuser@localhost
+> Subject: THIS IS NOT A TEST
+>
+> A song by Bikini Kill
+
+cat email.txt | /path/to/sendmail-to-matrix
+
+
+
+{
+  "server": "https://example.com",
+  "token": "tokenfromelementetc",
+  "room": "!DOESBETTERWITHTHEACTUALNAME:example.com",
+  "preface": "agaetr: "
+}
