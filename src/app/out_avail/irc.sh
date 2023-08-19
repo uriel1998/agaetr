ea06ab7624ab2310997e0e9b47c3e2776f99dcfe
Steven Saus <steven@stevesaus.com>
2023-08-19 05:57:16 -0400

    I am realizing that by switching the control to the ini files, I can actually put the functions all together and save space.

--- src/app/out_avail/irc.sh
+++ src/app/out_avail/irc.sh
@@ -8,16 +8,6 @@ echo -e 'USER bot guest tolmoon tolsun\nNICK bot\nJOIN #channel\nPRIVMSG #channe
 | nc irc.freenode.net 6667
 
 In this command, nc does the network connection, and you send a login info, nick, join a channel named "#channel" amd send a message "Ahoj lidi!" to that channel. And quit the server.
-Share
-Improve this answer
-Follow
-edited Jul 17, 2018 at 21:08
-answered Apr 14, 2011 at 22:18
-Ondra Žižka's user avatar
-Ondra Žižka
-43422 gold badges55 silver badges1414 bronze badges
+
 
     add \nQUIT at the end of the list of commands to quit right after sending the one message – 
-    Walter Heck
-    Dec 23, 2011 at 10:10
-
--- src/app/out_avail/wallabag.sh
+++ src/app/out_avail/wallabag.sh
@@ -11,9 +11,11 @@
 function wallabag_send {
     
     binary=$(grep 'wallabag =' "${XDG_CONFIG_HOME}/agaetr/agaetr.ini" | sed 's/ //g' | awk -F '=' '{print $2}')
-
-    outstring=$(echo "$binary add --quiet --title \"$title\" $link ")
-    echo "$outstring"
+    if [ ! -f "$binary" ];then
+        binary=$(which wallabag)
+    fi
+    outstring=$(echo "$binary add --title \"$title\" $link ")
+    #echo "$outstring"
     eval ${outstring} > /dev/null
 }
 
